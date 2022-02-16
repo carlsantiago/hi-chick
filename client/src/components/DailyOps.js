fd848250d08d796c01e811be85c8a470f7b6a047
Carl Santiago <4518gg@gmail.com>
2022-02-16 18:50:33 +1030

    Added formik functionality

--- client/src/components/DailyOps.js
+++ client/src/components/DailyOps.js
@@ -1,9 +1,7 @@
 import React from "react";
 import {
-  Button,
   FormControl,
   FormLabel,
-  Stack,
   InputGroup,
   InputLeftAddon,
   Select,
@@ -12,71 +10,150 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Button,
+  ModalFooter,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVenus, faMars } from "@fortawesome/free-solid-svg-icons";
 import { QUERY_FLOCKS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
+import { useFormik } from "formik";
+import { postDb } from "../utils/indexedDB";
 
 const DailyOps = () => {
   const { loading, data } = useQuery(QUERY_FLOCKS);
   console.log(data);
   const flocks = data?.flocks || [];
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const formik = useFormik({
+    initialValues: {
+      flock: "",
+      eggs: "",
+      mortsFemale: "",
+      mortsMale: "",
+    },
+    onSubmit: (values) => {
+      if (!values.flock) {
+        console.log("NO FLOCK");
+      } else {
+        onClose();
+      }
+      console.log(values);
+    },
+  });
+
+  console.log(formik.errors);
   return (
-    <Stack spacing="3">
-      <FormControl isRequired>
-        <FormLabel htmlFor="flock"> Flock </FormLabel>
-        <Select id="flock" placeholder="Please select one" size="lg">
-          {flocks &&
-            flocks.map((flock) => (
-              <option value={flock._id} key={flock._id}>
-                {flock.shed.location} - {flock.breed.name}
-              </option>
-            ))}
-        </Select>
-      </FormControl>
+    <>
+      <Button onClick={onOpen}>Daily Operations</Button>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Event</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <form onSubmit={formik.handleSubmit}>
+                <FormControl isRequired>
+                  <FormLabel htmlFor="flock"> Flock </FormLabel>
+                  <Select
+                    id="flock"
+                    name="flock"
+                    value={formik.values.flock}
+                    onChange={formik.handleChange}
+                    placeholder="Please select one"
+                    size="lg"
+                  >
+                    {flocks &&
+                      flocks.map((flock) => (
+                        <option value={flock._id} key={flock._id}>
+                          {flock.shed.location} - {flock.breed.name}
+                        </option>
+                      ))}
+                  </Select>
+                </FormControl>
 
-      <FormLabel htmlFor="eggs"> Eggs Collected </FormLabel>
-      <NumberInput min={0}>
-        <NumberInputField id="eggs" />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+                <FormLabel htmlFor="eggs"> Eggs Collected </FormLabel>
+                <NumberInput min={0}>
+                  <NumberInputField
+                    id="eggs"
+                    name="eggs"
+                    value={formik.values.eggs}
+                    onChange={formik.handleChange}
+                  />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
 
-      <FormLabel htmlFor="morts"> Mortality </FormLabel>
+                <FormLabel htmlFor="morts"> Mortality </FormLabel>
 
-      <InputGroup>
-        <InputLeftAddon
-          fontSize="1.2em"
-          children={<FontAwesomeIcon icon={faVenus} />}
-        />
+                <InputGroup>
+                  <InputLeftAddon
+                    fontSize="1.2em"
+                    children={<FontAwesomeIcon icon={faVenus} />}
+                  />
 
-        <NumberInput min={0} w="100%">
-          <NumberInputField id="mortsFemale" borderLeftRadius="0" />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </InputGroup>
+                  <NumberInput min={0} w="100%">
+                    <NumberInputField
+                      id="mortsFemale"
+                      name="mortsFemale"
+                      value={formik.values.mortsFemale}
+                      onChange={formik.handleChange}
+                      borderLeftRadius="0"
+                    />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </InputGroup>
 
-      <InputGroup>
-        <InputLeftAddon
-          fontSize="1.2em"
-          children={<FontAwesomeIcon icon={faMars} />}
-        />
+                <InputGroup>
+                  <InputLeftAddon
+                    fontSize="1.2em"
+                    children={<FontAwesomeIcon icon={faMars} />}
+                  />
 
-        <NumberInput min={0} w="100%">
-          <NumberInputField id="mortsMale" borderLeftRadius="0" />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </InputGroup>
-    </Stack>
+                  <NumberInput min={0} w="100%">
+                    <NumberInputField
+                      id="mortsMale"
+                      name="mortsMale"
+                      value={formik.values.mortsMale}
+                      onChange={formik.handleChange}
+                      borderLeftRadius="0"
+                    />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </InputGroup>
+
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} value="save" type="submit">
+                    Save
+                  </Button>
+                  <Button onClick={onClose}>Cancel</Button>
+                </ModalFooter>
+              </form>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      )}
+    </>
   );
 };
 
--- client/src/pages/DataEntry.js
+++ client/src/pages/DataEntry.js
@@ -4,30 +4,12 @@ import { Redirect } from "react-router-dom";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 import DailyOps from "../components/DailyOps";
-import {
-  Box,
-  Button,
-  VStack,
-  Stack,
-  Text,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Box, VStack, Stack, Text } from "@chakra-ui/react";
 import { QUERY_FLOCKS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
-import { postDb } from "../utils/indexedDB";
 
 const DataEntry = () => {
   const { loading, data } = useQuery(QUERY_FLOCKS);
-  console.log(data);
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   if (Auth.loggedIn()) {
     return (
@@ -40,69 +22,7 @@ const DataEntry = () => {
             <Text> Add Event</Text>
             <Box height="380px" width="100%">
               <VStack>
-                <Button onClick={onOpen}>Daily Operations</Button>
-
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Add Event</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                      <DailyOps />
-                    </ModalBody>
-
-                    <ModalFooter>
-                      <Button colorScheme="blue" mr={3} type="submit">
-                        Save
-                      </Button>
-                      <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-
-                <Button width="100%" onClick={onOpen}>
-                  Vaccination
-                </Button>
-
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Add Event</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                      <DailyOps />
-                    </ModalBody>
-
-                    <ModalFooter>
-                      <Button colorScheme="blue" mr={3}>
-                        Save
-                      </Button>
-                      <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-
-                <Button width="100%" onClick={onOpen}>
-                  Weighing
-                </Button>
-
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Add Event</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                      <DailyOps />
-                    </ModalBody>
-
-                    <ModalFooter>
-                      <Button colorScheme="blue" mr={3}>
-                        Save
-                      </Button>
-                      <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
+                <DailyOps />
               </VStack>
             </Box>
           </Stack>
